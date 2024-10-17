9abfb1029aa6012d797e48befe160c4e825d8cdd
doristrungvuong@gmail.com <doristrungvuong@gmail.com>
2024-10-17 01:47:46 -0400

    added config

--- p4/flask_app/config.py
+++ p4/flask_app/config.py
@@ -1,3 +1,3 @@
 # Stores all configuration values
-SECRET_KEY = ''
-MONGODB_HOST = ''
+SECRET_KEY = b'f\xb2\xe2\x9a>K(\x82Pc\xc9\x82\xc2P\xce0'
+MONGODB_HOST = 'mongodb+srv://nn88random:XMQmSmmLoi0NIhuV@cluster0.6crpc.mongodb.net/nhat_p4_database?retryWrites=true&w=majority&appName=Cluster0'
