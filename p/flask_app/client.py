8722009472c36d726ca2675632c2f32acc4b89a7
doristrungvuong@gmail.com <doristrungvuong@gmail.com>
2024-11-16 16:47:27 -0500

    made 2 classes: Course and Client to use planetterp api. can run main to test api

--- p/flask_app/client.py
+++ p/flask_app/client.py
@@ -1,108 +1,92 @@
 import requests
+# import planetterp as terp
+import re
 
+'''
+Separeate input string for search into 2 groups: Department and Course Number. 
+If both are found, return the reformatted string for a course.
+Else, return the reformatted string (upper case and no space)
+'''
+def separate_string(input_string):
+    uppercase_string = input_string.upper()
+    match = re.match(r"([A-Za-z]+)\s*(\d+)", uppercase_string)
+    if match:
+        letters = match.group(1)
+        numbers = match.group(2)
+        return letters + numbers, "Course"
+    else:
+        return uppercase_string, "Dept"
 
-class Movie(object):
-    def __init__(self, omdb_json, detailed=False):
-        if detailed:
-            self.genres = omdb_json["Genre"]
-            self.director = omdb_json["Director"]
-            self.actors = omdb_json["Actors"]
-            self.plot = omdb_json["Plot"]
-            self.awards = omdb_json["Awards"]
-
-        self.title = omdb_json["Title"]
-        self.year = omdb_json["Year"]
-        self.imdb_id = omdb_json["imdbID"]
-        self.type = "Movie"
-        self.poster_url = omdb_json["Poster"]
+class Course(object):
+    def __init__(self, terp_json):
+        self.name = terp_json["name"]
+        self.title = terp_json["title"]
+        self.credits = terp_json["credits"]
+        self.description = terp_json["description"]
+        self.type = "Course"
 
     def __repr__(self):
-        return self.title
+        return self.name
 
 
-class MovieClient(object):
-    def __init__(self, api_key):
+class Client(object):
+    def __init__(self):
         self.sess = requests.Session()
-        self.base_url = f"http://www.omdbapi.com/?apikey={api_key}&r=json&type=movie&"
 
     def search(self, search_string):
         """
         Searches the API for the supplied search_string, and returns
-        a list of Media objects if the search was successful, or the error response
+        a list of Course objects if the search was successful, or the error response
         if the search failed.
 
         Only use this method if the user is using the search bar on the website.
         """
-        search_string = "+".join(search_string.split())
-        page = 1
-
-        search_url = f"s={search_string}&page={page}"
-
-        resp = self.sess.get(self.base_url + search_url)
-
-        if resp.status_code != 200:
-            raise ValueError(
-                "Search request failed; make sure your API key is correct and authorized"
-            )
-
-        data = resp.json()
-
-        if data["Response"] == "False":
-            raise ValueError(f'[ERROR]: Error retrieving results: \'{data["Error"]}\' ')
-
-        search_results_json = data["Search"]
-        remaining_results = int(data["totalResults"])
+        reformatted_string, label = separate_string(search_string)
+        if label == "Course":
+            data = requests.get(f"https://api.planetterp.com/v1/course?name={x}").json() #course
+        else:
+            data = requests.get(f"https://api.planetterp.com/v1/courses?department={x}&limit=3").json() #department, limit 3 courses
 
         result = []
-
+        
+        if 'error' in data and data["error"] == "course not found":
+            raise ValueError(f"course {x} not found")
+        
         ## We may have more results than are first displayed
-        while remaining_results != 0:
-            for item_json in search_results_json:
-                result.append(Movie(item_json))
-                remaining_results -= len(search_results_json)
-            page += 1
-            search_url = f"s={search_string}&page={page}"
-            resp = self.sess.get(self.base_url + search_url)
-            if resp.status_code != 200 or resp.json()["Response"] == "False":
+        for terp_json in data:
+            if 'error' in data and data["error"] == "course not found":
                 break
-            search_results_json = resp.json()["Search"]
+            result.append(Course(terp_json))
 
         return result
 
-    def retrieve_movie_by_id(self, imdb_id):
+    def retrieve_course_by_id(self, course_name):
         """
-        Use to obtain a Movie object representing the movie identified by
-        the supplied imdb_id
+        Use to obtain a Course object representing the course identified by
+        the supplied course_name
         """
-        movie_url = self.base_url + f"i={imdb_id}&plot=full"
+        reformatted_course_name, _ = separate_string(course_name)
+        data = requests.get(f"https://api.planetterp.com/v1/course?name={reformatted_course_name}").json() #course
 
-        resp = self.sess.get(movie_url)
+        if 'error' in data and data["error"] == "course not found":
+            raise ValueError(f"course {x} not found")
 
-        if resp.status_code != 200:
-            raise ValueError(
-                "Search request failed; make sure your API key is correct and authorized"
-            )
+        course = Course(data)
 
-        data = resp.json()
-
-        if data["Response"] == "False":
-            raise ValueError(f'[ERROR]: Error retrieving results: \'{data["Error"]}\' ')
-
-        movie = Movie(data, detailed=True)
-
-        return movie
+        return course
 
 
 ## -- Example usage -- ###
 if __name__ == "__main__":
-    import os
+    x = "CMSC"
+    y = "CMSC132"
+    
+    client = Client()
+    results = client.search(x)
+    print(len(results))
+    print(results)
+    course = client.retrieve_course_by_id(y)
+    print(course)
+    print(course.description)
 
-    client = MovieClient(os.environ.get("OMDB_API_KEY"))
-
-    movies = client.search("guardians")
-
-    for movie in movies:
-        print(movie)
-
-    print(len(movies))
-    # test git changes 
+    
