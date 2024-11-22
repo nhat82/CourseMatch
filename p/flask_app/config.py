09301dc3d4c51e83afc86d1565f19851ce405f38
doristrungvuong@gmail.com <doristrungvuong@gmail.com>
2024-11-22 12:51:55 -0500

    connected mongodb, can see account - update username and profile pic

--- p/flask_app/config.py
+++ p/flask_app/config.py
@@ -1,3 +1,4 @@
 # Stores all configuration values
 SECRET_KEY = b'f\xb2\xe2\x9a>K(\x82Pc\xc9\x82\xc2P\xce0'
-MONGODB_HOST = 'mongodb+srv://nn88random:XMQmSmmLoi0NIhuV@cluster0.6crpc.mongodb.net/nhat_p4_database?retryWrites=true&w=majority&appName=Cluster0'
+MONGODB_HOST = 'mongodb+srv://nn88random:CIrvD08SGbew9j8V@cluster0.hjdic.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+# pass = CIrvD08SGbew9j8V
