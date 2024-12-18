070195e9da79b62d3fba43d4caa67d3ea5455e5a
Shasank Patel <77682340+corruptcookie@users.noreply.github.com>
2024-12-18 15:56:08 -0500

    add/remove/detail

--- p/flask_app/forms.py
+++ p/flask_app/forms.py
@@ -94,4 +94,4 @@ class FollowForm(FlaskForm):
     
 class UnfollowForm(FlaskForm):
     # can only unfollow if it's in the current user's list of following people
-    submit_unfollow = SubmitField("UnFollow")
+    submit_unfollow = SubmitField("Unfollow")
